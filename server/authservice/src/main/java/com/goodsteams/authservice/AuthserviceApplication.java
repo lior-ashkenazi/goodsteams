375918ee4bb2d25664e5ad8a64bc28eb088b90f4
Lior Ashkenazi <lior.ashkenazi>
2023-08-25 20:40:03 +0300

    Fixed a security error

--- server/authservice/src/main/java/com/goodsteams/authservice/AuthserviceApplication.java
+++ server/authservice/src/main/java/com/goodsteams/authservice/AuthserviceApplication.java
@@ -1,8 +1,11 @@
 package com.goodsteams.authservice;
 
+import com.goodsteams.authservice.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class AuthserviceApplication {
 
